ae7c2033cee3cbab774d41e7553261a316f314db
rokas <rokasra@gmail.com>
2022-06-09 15:02:03 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navigation from './components/Navigation';
+import AddPage from './pages/Add';
+import HomePage from './pages/Home';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Heloo again world</h1>
+      <Navigation />
+      <Switch>
+        <Route path='/home'>
+          <HomePage />
+        </Route>
+        <Route path='/add'>
+          <AddPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
